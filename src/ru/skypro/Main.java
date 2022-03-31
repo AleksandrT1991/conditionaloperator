135b5bbeaf651a773d879497829793d06f0ffab2
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-03-31 21:25:24 +0300

    поехали задание 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,20 @@
 package ru.skypro;
 
+import static java.lang.System.out;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// задание 1
+        int clientOS = 0;
+        if (clientOS <= 1) {
+            out.println(" Установите прежнюю версию приложения для Android по ссылке ");
+        }
+        else {
+            out.println(" Установите прежнюю версию приложения для IOS по ссылке ");
+        }
+
+
+
     }
 }
